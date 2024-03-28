1bc1eb1e1435c309f80e46bbffcf8dec24e2d77e
DoniaBatool <donia1510aptech@gmail.com>
2024-03-28 04:01:59 -0700

    WordCounter first commit

---
+++ wordCounter.ts
@@ -0,0 +1,62 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+let counter=(text:string)=>text.replace(/\s/g,"").length
+let isAvailable=true
+async function wordCount(counter:(word:string)=>number){
+    console.log(`
+    ██     ██ ███████ ██       ██████  ██████  ███    ███ ███████ 
+    ██     ██ ██      ██      ██      ██    ██ ████  ████ ██      
+    ██  █  ██ █████   ██      ██      ██    ██ ██ ████ ██ █████   
+    ██ ███ ██ ██      ██      ██      ██    ██ ██  ██  ██ ██      
+     ███ ███  ███████ ███████  ██████  ██████  ██      ██ ███████ 
+                                                                  
+                                                                  
+    `)
+    
+        
+    
+        let response= await inquirer.prompt({
+          type:"input",
+          name:"text",
+          message:(chalk.greenBright("Start Writing Here..."))
+    
+ })
+ 
+ console.log(counter(response.text))
+ 
+ while (isAvailable) {  
+
+ let response2= await inquirer.prompt({
+    type:"list",
+    name:"option",
+    message:"Would you like to continue or exit?",
+    choices:["Continue","Exit"]
+})
+console.log(response2.option)
+
+
+if (response2.option=="Continue"){
+    let response= await inquirer.prompt({
+        type:"input",
+        name:"text",
+        message:(chalk.bold.greenBright("Start Writing Here..."))
+    
+})
+console.log(counter(response.text))
+
+}
+
+else{
+    
+    console.log("GoodBye!")
+    
+    isAvailable=false
+    
+}}
+
+
+}
+
+wordCount(counter)
